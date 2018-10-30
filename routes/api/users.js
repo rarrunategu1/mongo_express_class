35b3c22e1efc9a5017258f52e788bc7483aefc64
Rose Parker <parker.rose@ymail.com>
2018-10-30 01:32:48 +0000

    push

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,4 +10,33 @@ router.get('/', (req, res)=> {
   .catch(err => console.log(err));
 });
 
+//url /api/users/?name=cliff
+//url /api/users/cliff?comment=id
+router.get("/:name", (req, res) => {
+  const name = req.params.name;
+  User.find({ name: name })
+  .then(user => {
+    if ( user === []) {
+       return res.status(404).json({message: `User: ${name} not found`})
+      }else{
+      res.json(user)
+  }
+  })
+  .catch(err => res.status(500).json({message: err})); //500 means something happened on server
+})
+
+router.post("/", (req, res) => {
+  const { name, password, avatar } = req.body;
+  
+  const newUser = new User({
+    name,
+    password,
+    avatar
+  })
+  
+  //newUser.avatar = avatar //add other attributes after creating that new item
+  
+  newUser.save().then
+})
+
 module.exports = router;
