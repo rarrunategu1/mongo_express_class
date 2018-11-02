5e80d293ab7f84bbf30c1a772486b8c059d5c42a
Rose Parker <parker.rose@ymail.com>
2018-11-02 19:23:03 +0000

    new profile Schema

--- models/Profile.js
+++ models/Profile.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProfileSchema = new Schema ({
-    fullName: {
-        firstName: {type: String, required: true},
-        lastName: {type: String, required: true}
-    },
-    lastName: {
-        type: String,
+    
+    firstname: {
+        type: String, 
         required: true
     },
-    aboutMe: {
+    lastname: {
+        type: String, 
+        required: true
+    },
+    aboutme: {
         type: String
     },
     age: {
@@ -22,13 +23,11 @@ const ProfileSchema = new Schema ({
         required:  true,
         unique: true
         
-    }, 
+    } 
 
 });
 
-ProfileSchema.virtual('fullname').get(function(){
-    return [this.firstName, this.lastName].filter(Boolean).join(' ');
-});
+
 
 module.exports = Profile = mongoose.model('profiles', ProfileSchema);
 
