feadfe31200da45a2b7fa7c259dda4acdc8aeb41
Rose Parker <parker.rose@ymail.com>
2018-11-02 19:24:12 +0000

    new router for profiles schema, added route to read all profiles andfind one profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -12,19 +12,34 @@ router.get('/', (req, res)=> {
 
 //reads one particular profile
 
-router.get('/:fullName', (req, res) =>{
-    const fullName = req.params.fullName;
-    Profile.findOne()
+router.get('/:email', (req, res) =>{
+    const email = req.params.email; //not good practice to use email as a param
+    Profile.findOne({email})
     .then(profile => {
         if (!profile){
             res.status(404)
-            .json({message: `The profile for email address ${fullName} does not exist`});
+            .json({message: `The profile for email address ${email} does not exist`});
         } else {
             res.json(profile);
         }
     })
     .catch(err => res.status(500).json({message: err}));
     
+    
+router.post("/", (req, res) => {
+    
+    const { firstname, lastname, email } = req.body;
+    
+    const newProfile = new Profile({
+        firstname,
+        lastname,
+        email
+    })
+
+    newProfile.save()
+        .then(profile =>res.status(202).json(profile))
+        .catch(err =>res.status(500).json({message: err}))
+})    
 });
 
 module.exports = router;
